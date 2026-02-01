9f0a7ce8cd529b49a8e3d2c35f6fe78417bc4d6b
CHOCOBO_DY\danie <YeungDan@Sheridancollege.ca>
2026-02-01 14:43:13 -0500

    Added sound effects to the duck.

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,6 +7,9 @@ public class HealthBar : MonoBehaviour
     public Slider healthBar;
         public SpriteRenderer player;
     public int health = 5;
+    public AudioSource audioSource;
+    public AudioClip sqeak;
+    public AudioClip death;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,13 +20,24 @@ public class HealthBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //audio souce stops and restarts
+        //play one shot is adding onto the already played sound
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         if (player.bounds.Contains(mousePos) && Mouse.current.leftButton.wasPressedThisFrame)
         {
             health -= 1;
+            ;
             if (health == 0)
             {
+                audioSource.clip = death;
+                audioSource.Play();
+                //audioSource.PlayOneShot(death);
                 gameObject.SetActive(false);
+            }
+            else
+            {
+                audioSource.clip = sqeak;
+                audioSource.Play();
             }
         }
         healthBar.value = health;
