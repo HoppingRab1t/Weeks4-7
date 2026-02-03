7bd876549a6c5b17d8c200bae01cb9b0949789e5
Daniel Yeung <yeungdan@shernet.sheridancollege.ca>
2026-02-03 10:01:53 -0500

    Checked out how to not make things go onto the buttom

---
+++ Assets/Scripts/duckie.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class duckie : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Mouse.current.leftButton.wasPressedThisFrame == true && EventSystem.current.IsPointerOverGameObject() == false)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            transform.position = mousePos;  
+        }
+    }
+}
