b19d43f963f09fcd03bbbddc296dea1a893e92d2
CHOCOBO_DY\danie <YeungDan@Sheridancollege.ca>
2026-02-15 15:42:26 -0500

    Added UI elements

--- Assets/Toy Project/Track Movement.cs
+++ Assets/Toy Project/Track Movement.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class TrackMovement : MonoBehaviour
 {
+    public bool track_move = true;
+    public int speed;
+    Vector2 pos;
+
+    Vector2 bottomLeft;
+    Vector2 topRight;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    
     }
 
     // Update is called once per frame
     void Update()
     {
+        pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+
+        if (pos.x >= topRight.x)
+        {
+            pos.x = -13;
+        }
+
+        if (track_move == true)
+        {
+            transform.position = pos;
+        }
         
     }
 }
